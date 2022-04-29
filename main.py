4ce72ae0bfade2f6805b861c4e44ee6008bca6df
HoshinoShinichi <hoshino@dds.co.jp>
2022-04-29 17:55:13 +0900

    CSV処理用エンドポイント定義

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template, make_response, jsonify, request
 import functools
 import json
+import csv
+import io
 import base64
 import binascii
 
@@ -80,6 +82,48 @@ def ep_base64decode():
 @app.route("/b64", methods=["GET"])
 def ep_base64_tool():
     return render_template('b64.html')
+
+
+
+
+
+
+# csv test
+#
+# content-type : application/json
+# body {"data":"[Base64 encoded CSV]"}
+@app.route("/csv", methods=["POST"])
+@content_type('application/json')
+def ep_csv():
+    print(request.json['data'])
+    csv_b64 = request.json['data']
+
+    # BASE64デコード
+    decode_csv = base64.b64decode(csv_b64).decode()
+    print(decode_csv)
+
+    # 文字列をストリームへ書き込み
+    f = io.StringIO()
+    f.write(decode_csv)
+    f.seek(0)
+
+    # CSVとしてパース
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+        # TODO: CSVに対する任意の処理
+        print(row)
+
+    f.close()
+
+    # make result
+    result_data = ""
+
+    # make response
+    j = json.loads('{"result":""}')
+    j["result"] = result_data
+
+    # response
+    return j
 
 
 
